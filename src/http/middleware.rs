2d2a465b7651f0f186352fb8306c4a2907dd0f27
Henri Martikainen <henri.martikainen@protonmail.com>
2024-05-11 15:55:50 -0500

    content-encoding middleware updated to handle lists

--- src/http/middleware.rs
+++ src/http/middleware.rs
@@ -3,19 +3,15 @@ use crate::http::{request::Request, State};
 use super::{header::Headers, router::Handler};
 
 pub fn content_encoding(handler: Handler) -> Handler {
-    Box::new(move |request: Request, state: State| { 
-        let encoding = request
-            .metadata
-            .headers
-            .get("Accept-Encoding")
-            .map(str::to_string);
-        let mut content_encoding = None;
-
-        if let Some(encoding) = encoding {
-            if state.supported_encoding(encoding.as_str()) {
-                content_encoding = Some(encoding);
-            }
-        }
+    Box::new(move |request: Request, state: State| {
+        let content_encoding = match request.metadata.headers.get("Accept-Encoding") {
+            Some(encodings) => encodings
+                .split(",")
+                .map(|s| s.trim())
+                .find(|encoding| state.supported_encoding(encoding))
+                .map(str::to_string),
+            None => None,
+        };
 
         let resp = handler(request, state);
         if let Some(content_encoding) = content_encoding {
